e7fa2c89720ba4df44fc31bbee12452eb8a93ad3
NurAhissami <nurahi10@gmail.com>
2021-05-12 10:50:49 +0200

    Add color winner

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const inputNumber = document.querySelector('.js-input');
 const inputButton = document.querySelector('.js-button');
 const trackInfo = document.querySelector('.js-track');
 const triedNumber= document.querySelector('.js-tried');
-
+const bodyBack = document.querySelector('.js-body');
 
 function getRandomNumber(max) {
     return Math.ceil(Math.random() * max);
@@ -19,14 +19,15 @@ console.log(randomNumber)
 function compareNumber (){
    
     const inputValue = parseInt(inputNumber.value);
-    console.log(inputValue, randomNumber)
+    console.log(inputValue, randomNumber);
 
     if (inputValue === randomNumber){
 
     trackInfo.innerHTML = 'Enhorabuena!! has ganado! =) ¡recarga la página y juega otra vez!';
     inputButton.setAttribute('disabled', false);
     inputButton.classList.add('colordisable')
-    
+bodyBack.classList.add('colorback')
+trackInfo.classList.add('colortext')
     }
     else if (inputValue > randomNumber && inputValue <= 100){
         trackInfo.innerHTML = 'WOW, ¡te has pasado!'
